901ab4366691905048ff37a49bf34fdde54346e9
AnthoniBortolotto <antho@outlook.com.br>
2023-09-10 15:57:39 -0300

    dependencies missing to deploy installed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ChatController } from './chat/Controller/chat.controller';
 import { ChatModule } from './chat/Module/chat.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env',
-  }), ChatModule ],
-  controllers: [AppController, ChatController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ChatModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
