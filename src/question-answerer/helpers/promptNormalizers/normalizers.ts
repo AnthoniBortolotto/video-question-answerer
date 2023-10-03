d6d52f062d4b353c2557af646eed8a5f6608e0dd
AnthoniBortolotto <antho@outlook.com.br>
2023-10-03 00:13:22 -0300

    text splitting improved

--- src/question-answerer/helpers/promptNormalizers/normalizers.ts
+++ src/question-answerer/helpers/promptNormalizers/normalizers.ts
@@ -5,22 +5,25 @@
  * @returns splitted prompt
  */
 export function splitPrompts(prompt: string, chunkLimit: number): string[] {
-    const splitedPrompt = prompt.split('\n');
-    let currentChunk = new Array<string>();
-  
-    const promptChunks = new Array<string>();
-    splitedPrompt.map((chunk, i) => {
-        if (currentChunk.length + chunk.length < chunkLimit) {
-          currentChunk.push(chunk);
-        }
-        else {
-          promptChunks.push(currentChunk.join(' '));
-          currentChunk = new Array<string>();
-          currentChunk.push(chunk);
-        }
-    });
-    promptChunks.push(currentChunk.join(' '));
-    return promptChunks;
+  const splitedPrompt = prompt.split('\n');
+  let currentChunk = new Array<string>();
+  let currentChunkSize = 0;
+
+
+  const promptChunks = new Array<string>();
+  splitedPrompt.map((chunk, i) => {
+    if (currentChunkSize + chunk.length < chunkLimit) {
+      currentChunk.push(chunk);
+      currentChunkSize += chunk.length;
+    } else {
+      promptChunks.push(currentChunk.join(' '));
+      currentChunk = new Array<string>();
+      currentChunkSize = 0;
+      currentChunk.push(chunk);
+    }
+  });
+  promptChunks.push(currentChunk.join(' '));
+  return promptChunks;
 }
 
 export function sanitizePrompt(prompt: string): string {
